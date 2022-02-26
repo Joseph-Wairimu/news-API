237dcd7136a10e67c6981970695ef13daae1ca08
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-26 14:09:00 +0300

    get data from our api

--- app/request.py
+++ app/request.py
@@ -1,4 +1,17 @@
+# from app import app
+
+# # Getting api key
+# api_key = app.config['NEWS_API_KEY']
+
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.news
+
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
