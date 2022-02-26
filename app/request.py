932b366c0a20f5ae0382cea6ad2a8aea7124a1ec
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-26 15:24:13 +0300

    display news sources

--- app/request.py
+++ app/request.py
@@ -3,7 +3,7 @@ from app import app
 import urllib.request,json
 from .models import news
 
-News = news.news
+News = news.News
 
 
 # Getting api key
@@ -13,24 +13,24 @@ api_key = app.config['NEWS_API_KEY']
 base_url = app.config["NEWS_API_BASE_URL"]
 
 
-def get_news(category):
+def get_news():
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_news_url = base_url.format(api_key)
 
     with urllib.request.urlopen(get_news_url) as url:
        get_news_data = url.read()
        get_news_response = json.loads(get_news_data)
 
-        news_results = None
+       news_results = None
 
-        if get_news_response['results']:
-            news_results_list = get_news_response['results']
-            news_results = process_results(news_results_list)
+       if get_news_response['sources']:
+        news_results_list = get_news_response['sources']
+        news_results = process_results(news_results_list)
 
 
-    return news_results
+        return news_results
 
 
 
@@ -47,14 +47,14 @@ def process_results(news_list):
     news_results = []
     for news_item in news_list:
         id = news_item.get('id')
-        title = news_item.get('original_title')
-        overview = news_item.get('overview')
-        poster = news_item.get('poster_path')
-        vote_average = news_item.get('vote_average')
-        vote_count = news_item.get('vote_count')
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        language= news_item.get('language')
+        country = news_item.get('country')
 
-        if poster:
-            news_object = News(id,title,overview,poster,vote_average,vote_count)
+        if url:
+            news_object = News(id,name,description,url,language,country)
             news_results.append(news_object)
 
     return news_results
--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app import app
 
 # Views
+from .request import get_news
+
 @app.route('/')
 def index():
 
@@ -9,8 +11,12 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    # Getting popular movie
+    popular_news = get_news()
+    print(popular_news)
     title = 'Home - Welcome to The Best news broadcast Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular = popular_news)
+
 @app.route('/news/<int:news_id>')
 def news(news_id):
 
@@ -18,3 +24,4 @@ def news(news_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('news.html',id = news_id)
+
