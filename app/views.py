ef49811b5489fe3a845d92369dbf714a39c07f32
Joseph-Wairimu <josephkamore084@gmail.com>
2022-02-26 13:45:20 +0300

    add title

--- app/views.py
+++ app/views.py
@@ -8,8 +8,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
 
+    title = 'Home - Welcome to The Best news broadcast Online'
+    return render_template('index.html', title = title)
 @app.route('/news/<int:news_id>')
 def news(news_id):
 
